4c34d6039df4ea3e3662036c59d7fa7766b6d58d
rakshit.mentem <rakshit.mentem@gmail.com>
2024-01-04 17:08:46 +0530

    fetch data only

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,57 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import axios from 'axios'
-import './App.css'
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
-  console.log("helloo")
-  const callapidata = async () => {
+function show() {
+  const [api, setApi] = useState({
+    id: 0,
+    title: "",
+    description: "",
+    category: "",
+    image: "",
+    price: 0
+  });
+
+
+  const getapiProducts = async () => {
     try {
-      const call = await axios.get(`https://fakestoreapi.com/products`)
-      console.log(data.call)
+      const response = await axios.get(`https://fakestoreapi.com/products`);
+      console.log(response.data);
+      setApi(response.data);
     } catch (error) {
-      console.log("api not found")
+      console.error("error", error);
     }
-  }
-}
+  };
 
-export default App
+  useEffect(() => {
+    getapiProducts();
+  }, []);
+
+  return (
+    // <div className="container">
+    //   <div className="card">
+    //     <h1>product</h1>
+    //     <h1>{api.id}</h1>
+    //     <h1>{api.title}</h1>
+
+    //   </div>
+    // </div>
+    <div className="container">
+      <div className="card" style={{ width: '18rem' }}>
+        <img src="..." className="card-img-top" alt="Card" />
+        <div className="card-body">
+          <h1>{api.id}</h1>
+          <h2 className="card-title">{api.title}</h2>
+          <p className="card-text">
+            {api.description}
+          </p>
+          <a href="#" className="btn btn-primary">
+            select
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default show;
